bed8793aeca9b3297102d5893c334c48052671d1
l33tw33b <ronaldyungkuen@gmail.com>
2021-06-10 13:10:19 +0800

    Data maintenance

---
+++ GroupProject/DataMaintenance.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace GroupProject
+{
+    public partial class DataMaintenance : Form
+    {
+        private string connection = "server=localhost;user id=root;database=ede;password=root";
+
+        public DataMaintenance()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void cb_table_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            using (MySqlConnection mysqlcon = new MySqlConnection(connection)) {
+                try {
+                    mysqlcon.Open();
+                    string sql = "SELECT * FROM " + cb_table.Text + ";";
+                    MySqlCommand cmd = new MySqlCommand(sql, mysqlcon);
+                    MySqlDataAdapter da = new MySqlDataAdapter();
+                    da.SelectCommand = cmd;
+                    cmd.ExecuteNonQuery();
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+                    dataGridView1.DataSource = dt;
+                    mysqlcon.Close();
+                } catch (Exception ex) {
+                    MessageBox.Show(ex.Message);
+                }
+
+
+            }
+            
+            
+        }
+    }
+}
--- GroupProject/LoginForm.Designer.cs
+++ GroupProject/LoginForm.Designer.cs
@@ -38,13 +38,14 @@ namespace GroupProject
             this.lbl_Password = new System.Windows.Forms.Label();
             this.lbl_AccountName = new System.Windows.Forms.Label();
             this.btnTestUse = new System.Windows.Forms.Button();
+            this.btn_staff = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btn_Register
             // 
-            this.btn_Register.Location = new System.Drawing.Point(178, 178);
+            this.btn_Register.Location = new System.Drawing.Point(178, 193);
             this.btn_Register.Name = "btn_Register";
-            this.btn_Register.Size = new System.Drawing.Size(100, 27);
+            this.btn_Register.Size = new System.Drawing.Size(100, 29);
             this.btn_Register.TabIndex = 15;
             this.btn_Register.Text = "Register";
             this.btn_Register.UseVisualStyleBackColor = true;
@@ -52,9 +53,9 @@ namespace GroupProject
             // 
             // btn_Exit
             // 
-            this.btn_Exit.Location = new System.Drawing.Point(299, 178);
+            this.btn_Exit.Location = new System.Drawing.Point(299, 193);
             this.btn_Exit.Name = "btn_Exit";
-            this.btn_Exit.Size = new System.Drawing.Size(100, 27);
+            this.btn_Exit.Size = new System.Drawing.Size(100, 29);
             this.btn_Exit.TabIndex = 14;
             this.btn_Exit.Text = "Exit";
             this.btn_Exit.UseVisualStyleBackColor = true;
@@ -62,9 +63,9 @@ namespace GroupProject
             // 
             // btn_Login
             // 
-            this.btn_Login.Location = new System.Drawing.Point(57, 178);
+            this.btn_Login.Location = new System.Drawing.Point(57, 193);
             this.btn_Login.Name = "btn_Login";
-            this.btn_Login.Size = new System.Drawing.Size(100, 27);
+            this.btn_Login.Size = new System.Drawing.Size(100, 29);
             this.btn_Login.TabIndex = 13;
             this.btn_Login.Text = "Login";
             this.btn_Login.UseVisualStyleBackColor = true;
@@ -74,7 +75,7 @@ namespace GroupProject
             // 
             this.lbl_Title.AutoSize = true;
             this.lbl_Title.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_Title.Location = new System.Drawing.Point(55, 40);
+            this.lbl_Title.Location = new System.Drawing.Point(55, 43);
             this.lbl_Title.Name = "lbl_Title";
             this.lbl_Title.Size = new System.Drawing.Size(349, 23);
             this.lbl_Title.TabIndex = 12;
@@ -82,53 +83,64 @@ namespace GroupProject
             // 
             // tbx_Password
             // 
-            this.tbx_Password.Location = new System.Drawing.Point(138, 135);
+            this.tbx_Password.Location = new System.Drawing.Point(138, 146);
             this.tbx_Password.MaxLength = 16;
             this.tbx_Password.Name = "tbx_Password";
             this.tbx_Password.PasswordChar = '*';
-            this.tbx_Password.Size = new System.Drawing.Size(262, 22);
+            this.tbx_Password.Size = new System.Drawing.Size(262, 20);
             this.tbx_Password.TabIndex = 11;
             // 
             // tbx_AccountName
             // 
-            this.tbx_AccountName.Location = new System.Drawing.Point(138, 96);
+            this.tbx_AccountName.Location = new System.Drawing.Point(138, 104);
             this.tbx_AccountName.Name = "tbx_AccountName";
-            this.tbx_AccountName.Size = new System.Drawing.Size(262, 22);
+            this.tbx_AccountName.Size = new System.Drawing.Size(262, 20);
             this.tbx_AccountName.TabIndex = 10;
             // 
             // lbl_Password
             // 
             this.lbl_Password.AutoSize = true;
-            this.lbl_Password.Location = new System.Drawing.Point(55, 135);
+            this.lbl_Password.Location = new System.Drawing.Point(55, 146);
             this.lbl_Password.Name = "lbl_Password";
-            this.lbl_Password.Size = new System.Drawing.Size(51, 12);
+            this.lbl_Password.Size = new System.Drawing.Size(56, 13);
             this.lbl_Password.TabIndex = 9;
             this.lbl_Password.Text = "Password:";
             // 
             // lbl_AccountName
             // 
             this.lbl_AccountName.AutoSize = true;
-            this.lbl_AccountName.Location = new System.Drawing.Point(55, 99);
+            this.lbl_AccountName.Location = new System.Drawing.Point(55, 107);
             this.lbl_AccountName.Name = "lbl_AccountName";
-            this.lbl_AccountName.Size = new System.Drawing.Size(77, 12);
+            this.lbl_AccountName.Size = new System.Drawing.Size(81, 13);
             this.lbl_AccountName.TabIndex = 8;
             this.lbl_AccountName.Text = "Account Name:";
             // 
             // btnTestUse
             // 
-            this.btnTestUse.Location = new System.Drawing.Point(347, 12);
+            this.btnTestUse.Location = new System.Drawing.Point(347, 13);
             this.btnTestUse.Name = "btnTestUse";
-            this.btnTestUse.Size = new System.Drawing.Size(111, 23);
+            this.btnTestUse.Size = new System.Drawing.Size(111, 25);
             this.btnTestUse.TabIndex = 16;
             this.btnTestUse.Text = "Testing Quick Login";
             this.btnTestUse.UseVisualStyleBackColor = true;
             this.btnTestUse.Click += new System.EventHandler(this.btnTestUse_Click);
             // 
+            // btn_staff
+            // 
+            this.btn_staff.Location = new System.Drawing.Point(178, 230);
+            this.btn_staff.Name = "btn_staff";
+            this.btn_staff.Size = new System.Drawing.Size(100, 29);
+            this.btn_staff.TabIndex = 17;
+            this.btn_staff.Text = "Staff Login";
+            this.btn_staff.UseVisualStyleBackColor = true;
+            this.btn_staff.Click += new System.EventHandler(this.btn_staff_Click);
+            // 
             // LoginForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(470, 250);
+            this.ClientSize = new System.Drawing.Size(470, 271);
+            this.Controls.Add(this.btn_staff);
             this.Controls.Add(this.btnTestUse);
             this.Controls.Add(this.btn_Register);
             this.Controls.Add(this.btn_Exit);
@@ -157,5 +169,6 @@ namespace GroupProject
         private System.Windows.Forms.Label lbl_Password;
         private System.Windows.Forms.Label lbl_AccountName;
         private System.Windows.Forms.Button btnTestUse;
+        private System.Windows.Forms.Button btn_staff;
     }
 }
